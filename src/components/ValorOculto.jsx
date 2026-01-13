e5bbd3ac5f857b8ceb2beec92a141219b4b26465
MauroGeorgePtga <mauro_george@hotmail.com>
2026-01-13 13:47:15 -0400

    Add files via upload

---
+++ src/components/ValorOculto.jsx
@@ -0,0 +1,11 @@
+import { useVisibility } from '../contexts/VisibilityContext'
+
+export function ValorOculto({ valor, formatado = true }) {
+  const { valoresVisiveis } = useVisibility()
+
+  if (!valoresVisiveis) {
+    return <span>••••••</span>
+  }
+
+  return <span>{valor}</span>
+}
