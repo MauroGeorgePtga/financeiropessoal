ba1dcaa6a571eb49281b3477e78fb57799ef3954
MauroGeorgePtga <mauro_george@hotmail.com>
2026-01-16 08:17:40 -0400

    Update ValorOculto.jsx

--- src/components/ValorOculto.jsx
+++ src/components/ValorOculto.jsx
@@ -6,15 +6,3 @@ export function ValorOculto({ valor, formatado = true }) {
   if (!valoresVisiveis) {
     return <span>••••••</span>
   }
-
-  const valorFormatado = formatado 
-    ? new Intl.NumberFormat('pt-BR', {
-        style: 'currency',
-        currency: 'BRL',
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2
-      }).format(valor || 0)
-    : valor
-
-  return <span>{valorFormatado}</span>
-}
